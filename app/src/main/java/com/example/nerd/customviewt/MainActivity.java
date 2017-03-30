7aa9f4bb76eec734e4f60378edd3c990cc47dc96
xiecaiji <xiecaiji@beyondsoft.com>
2017-03-30 17:02:57 +0800

    optimization vercode

--- app/src/main/java/com/example/nerd/customviewt/MainActivity.java
+++ app/src/main/java/com/example/nerd/customviewt/MainActivity.java
@@ -2,12 +2,25 @@ package com.example.nerd.customviewt;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.nerd.verview.VerCode;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
+    VerCode ver_code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ver_code= (VerCode) findViewById(R.id.ver_code);
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,ver_code.getmTxt(),Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- verview/src/main/java/com/example/nerd/verview/VerCode.java
+++ verview/src/main/java/com/example/nerd/verview/VerCode.java
@@ -6,6 +6,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.os.Build;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -37,6 +38,8 @@ public class VerCode extends View {
     private Rect mBound;
     private Paint mPaint;
 
+    private Random mRandom;
+
     public VerCode(Context context) {
         this(context, null);
     }
@@ -51,8 +54,9 @@ public class VerCode extends View {
         mTxtColor = a.getColor(R.styleable.VerCode_txtColor, Color.BLACK);
         mTxtSize = a.getDimensionPixelSize(R.styleable.VerCode_txtSize, (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, 16, getResources().getDisplayMetrics()));
         mLeng = a.getInt(R.styleable.VerCode_verLen, 4);
-        mRadius=a.getFloat(R.styleable.VerCode_radius,0f);
+        mRadius = a.getFloat(R.styleable.VerCode_radius, 0f);
         a.recycle();
+        mRandom = new Random();
         mTxt = randomText();
 
         mPaint = new Paint();
@@ -73,21 +77,19 @@ public class VerCode extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         mPaint.setColor(Color.GRAY);
-        canvas.drawRoundRect(0, 0, getMeasuredWidth(), getMeasuredHeight(),mRadius,mRadius, mPaint);
-
+        mPaint.setStyle(Paint.Style.STROKE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP)
+            canvas.drawRoundRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mRadius, mRadius, mPaint);
+        else
+            canvas.drawRect(0, 0, getMeasuredWidth(), getMeasuredHeight(), mPaint);
         mPaint.setColor(mTxtColor);
-        Log.e("getWidth", getWidth() + "");
-        Log.e("bound.width", mBound.width() + "");
-        Log.e("getHeight", getHeight() + "");
-        Log.e("bound.height", mBound.height() + "");
-        Log.e("wid", getWidth() / 2 - mBound.width() / 2 + "");
-        Log.e("hei", getHeight() / 2 + mBound.height() / 2 + "");
         //x:字符串开始的x坐标 y:baseLine的位置
         Paint.FontMetricsInt fontMetricsInt = mPaint.getFontMetricsInt();
         int baseLine = (getMeasuredHeight() - fontMetricsInt.bottom + fontMetricsInt.top) / 2 - fontMetricsInt.top;
-        Log.e("baseLine", baseLine + "");
-        canvas.drawText(mTxt, getWidth() / 2 - mBound.width() / 2, baseLine, mPaint);
+//        canvas.drawText(mTxt, getWidth() / 2 - mBound.width() / 2, baseLine, mPaint);
+        drawRandomText(canvas, mPaint);
         addLines(canvas, mPaint);
+        addPoints(canvas, mPaint);
     }
 
     @Override
@@ -119,6 +121,28 @@ public class VerCode extends View {
         setMeasuredDimension(width, height);
     }
 
+    private void drawRandomText(Canvas canvas, Paint paint) {
+        float charLength = getWidth() / mLeng;
+
+        for (int i = 1; i <= mLeng; i++) {
+            int offsetDegree = mRandom.nextInt(15);
+            //这里只会产生0和1，如果是1那么正旋转正角度，否则旋转负角度
+            offsetDegree = mRandom.nextInt(2) == 1 ? offsetDegree : -offsetDegree;
+            //用来保存Canvas的状态。save之后，可以调用Canvas的平移、放缩、旋转、错切、裁剪等操作。
+            canvas.save();
+            //设置旋转
+            canvas.rotate(offsetDegree, getWidth() / 2, getHeight() / 2);
+            //给画笔设置随机颜色
+            mPaint.setARGB(255, mRandom.nextInt(200) + 20, mRandom.nextInt(200) + 20,
+                    mRandom.nextInt(200) + 20);
+            //设置字体的绘制位置
+            canvas.drawText(String.valueOf(mTxt.charAt(i - 1)), (i - 1) * charLength + 5,
+                    getHeight() * 4 / 5f, mPaint);
+            //用来恢复Canvas之前保存的状态。防止save后对Canvas执行的操作对后续的绘制有影响。
+            canvas.restore();
+        }
+    }
+
     private String randomText() {
         String chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
         StringBuilder stringBuilder = new StringBuilder("");
@@ -133,22 +157,39 @@ public class VerCode extends View {
     }
 
     private void addLines(Canvas canvas, Paint paint) {
-        Random random=new Random();
-        for (int i = 0; i < 80; i++) {
+        for (int i = 0; i < mLeng * 2; i++) {
             paint.setColor(Color.parseColor(getRandomColor()));
-            canvas.drawLine(random.nextInt(getMeasuredWidth()),random.nextInt(getMeasuredHeight()),random.nextInt(getMeasuredWidth()),random.nextInt(getMeasuredHeight()),mPaint);
+            canvas.drawLine(mRandom.nextInt(getMeasuredWidth()), mRandom.nextInt(getMeasuredHeight()), mRandom.nextInt(getMeasuredWidth()), mRandom.nextInt(getMeasuredHeight()), mPaint);
+        }
+    }
+
+    private void addPoints(Canvas canvas, Paint paint) {
+        paint.setStyle(Paint.Style.FILL);
+        for (int i = 0; i < mLeng * 5; i++) {
+            paint.setColor(Color.parseColor(getRandomColor()));
+//            paint.setStrokeWidth(random.nextInt(10));
+            canvas.drawCircle(mRandom.nextInt(getMeasuredWidth()), mRandom.nextInt(getMeasuredHeight()), mRandom.nextInt(8), paint);
         }
     }
 
     private String getRandomColor() {
         String r, g, b;
-        Random random = new Random();
-        r = Integer.toHexString(random.nextInt(256)).toUpperCase();
-        g = Integer.toHexString(random.nextInt(256)).toUpperCase();
-        b = Integer.toHexString(random.nextInt(256)).toUpperCase();
+        r = Integer.toHexString(mRandom.nextInt(256)).toUpperCase();
+        g = Integer.toHexString(mRandom.nextInt(256)).toUpperCase();
+        b = Integer.toHexString(mRandom.nextInt(256)).toUpperCase();
         r = r.length() == 1 ? "0" + r : r;
         g = g.length() == 1 ? "0" + g : g;
         b = b.length() == 1 ? "0" + b : b;
-        return "#"+r+g+b;
+        return "#" + r + g + b;
+    }
+
+    public String getmTxt() {
+        return mTxt;
+    }
+    public String getUppermTxt(){
+        return  mTxt.toUpperCase();
+    }
+    public String getLowermTxt(){
+        return mTxt.toLowerCase();
     }
 }
